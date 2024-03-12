bf8878225d8be8b85776efa640f1966816c32361
simha97 <62260118+simha97@users.noreply.github.com>
2024-03-12 09:44:25 +0100

    routing works

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://zczbffrkrnrupttvvbvo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjemJmZnJrcm5ydXB0dHZ2YnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNzc2MDUsImV4cCI6MjAyNTc1MzYwNX0.t4HDl0WdDRz2Vsg96yRsJn0x8OnzfHusEodWXUIoG78')
+const projects = ref([])
+
+async function getProjects() {
+  const { data } = await supabase.from('projects').select()
+  projects.value = data
+}
+
+onMounted(() => {
+  getProjects()
+})
+</script>
+
 <template>
-    <div>
-      <h1>Welcome to the Main Page</h1>
-      <v-btn variant="tonal">
-        Button
-        </v-btn>
-    </div>
-  </template>
-  
+  <ul>
+    <li v-for="project in projects" :key="project.id">
+      <NuxtLink :to="`/projects/${project.id}`">{{ project.name }}</NuxtLink>
+    </li>  </ul>
+</template>
---
+++ pages/projects/[id].vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { createClient } from '@supabase/supabase-js'
+
+const router = useRouter()
+const project = ref(null)
+
+const supabase = createClient('https://zczbffrkrnrupttvvbvo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjemJmZnJrcm5ydXB0dHZ2YnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNzc2MDUsImV4cCI6MjAyNTc1MzYwNX0.t4HDl0WdDRz2Vsg96yRsJn0x8OnzfHusEodWXUIoG78')
+
+async function fetchProject() {
+  const projectId = router.currentRoute.value.params.id
+  const { data, error } = await supabase
+    .from('projects')
+    .select('*')
+    .eq('id', projectId)
+    .single()
+
+  if (data) {
+    project.value = data
+  } else {
+    console.error(error)
+  }
+}
+
+onMounted(fetchProject)
+</script>
+
+<template>
+  <div v-if="project">
+    <h1>{{ project.name }}</h1>
+    <p>{{ project.date }}</p>
+    <p>{{ project.description }}</p>
+  </div>
+  <div v-else>
+    <p>Loading project...</p>
+  </div>
+</template>
