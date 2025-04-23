7ae56961cdbab523c6f18f70fbd37c47923b48cb
simha97 <62260118+simha97@users.noreply.github.com>
2025-04-23 21:43:48 +0200

    updated picture

--- data/websites.js
+++ data/websites.js
@@ -6,7 +6,7 @@ export const websites = ref([
       'https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=300&q=80',
   },
   {
-    name: 'TjenAI',
+    name: 'TjenAI!',
     url: 'https://tjenai.vercel.app/',
     imageUrl:
       'https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=300&q=80',
