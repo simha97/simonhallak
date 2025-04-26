5356fb4239c52c7cc0b2836803f0300ff9aca182
simha97 <62260118+simha97@users.noreply.github.com>
2025-04-26 15:02:14 +0200

    added new web

--- data/websites.js
+++ data/websites.js
@@ -2,19 +2,17 @@ export const websites = ref([
   {
     name: 'BookSpace',
     url: 'https://book-space2.vercel.app/',
-    imageUrl:
-      'https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=300&q=80',
   },
   {
     name: 'TjenAI!',
     url: 'https://tjenai.vercel.app/',
-    imageUrl:
-      'https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=300&q=80',
   },
   {
     name: 'SoundAsleep',
     url: 'https://soundasleep.vercel.app/',
-    imageUrl:
-      'https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=300&q=80',
+  },
+  {
+    name: 'WattSaver',
+    url: 'https://wattsaver-app.vercel.app/',
   },
 ]);
