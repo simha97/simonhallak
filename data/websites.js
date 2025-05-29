f9bd37e79805ed7a50c36a7ac73a7758c823a98b
simha97 <62260118+simha97@users.noreply.github.com>
2025-05-29 19:42:19 +0200

    added a new web

--- data/websites.js
+++ data/websites.js
@@ -1,5 +1,9 @@
 export const websites = ref([
   {
+    name: 'AskTheEmperor',
+    url: 'https://ask-the-emperor.vercel.app/',
+  },
+  {
     name: 'BookSpace',
     url: 'https://book-space2.vercel.app/',
   },
